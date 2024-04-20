bf9cfe993107d6fd6a5c7bab9229a77aed1fd32f
Pawan Kumar <ratwanapawan@gmail.com>
2024-04-20 21:20:13 +0530

    create login form

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const searchForm = document.querySelector('.search-form');
 const shoppinCart = document.querySelector('.shopping-cart');
+const cartLogin = document.querySelector('.cart-login');
 // to toggle the  search form bix
 document.querySelector('#search-btn').onclick = () => {
     searchForm.classList.toggle('active');
@@ -8,6 +9,9 @@ document.querySelector('#search-btn').onclick = () => {
 document.querySelector('#cart-btn').onclick = () => {
     console.log("Shopping cart click")
     shoppinCart.classList.toggle('active')
+}
+document.querySelector('#login-btn').onclick = () => {
+    cartLogin.classList.toggle('active')
 }
 
 // document.addEventListener('click', () => {
