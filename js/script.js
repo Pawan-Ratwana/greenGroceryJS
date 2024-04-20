f403c896b0abf1aeacb3b9ad04b73f53b4c3fcba
Pawan Kumar <ratwanapawan@gmail.com>
2024-04-20 19:46:20 +0530

    adding search-bar and cart-item in header

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const searchForm = document.querySelector('.search-form');
+const shoppinCart = document.querySelector('.shopping-cart');
+// to toggle the  search form bix
+document.querySelector('#search-btn').onclick = () => {
+    searchForm.classList.toggle('active');
+}
+
+document.querySelector('#cart-btn').onclick = () => {
+    console.log("Shopping cart click")
+    shoppinCart.classList.toggle('active')
+}
+
+// document.addEventListener('click', () => {
+//     searchForm.classList.remove('active');
+//     shoppinCart.classList.remove('active');
+//     console.log("local click")
+// })
+document.addEventListener('scroll', () => {
+    searchForm.classList.remove('active');
+    shoppinCart.classList.remove('active');
+})
