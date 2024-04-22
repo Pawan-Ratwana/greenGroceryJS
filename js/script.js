5f9b10e3fe95789963c5d1bb14c09fb471de39d1
Pawan Kumar <ratwanapawan@gmail.com>
2024-04-22 19:19:27 +0530

    complete the feature section

--- js/script.js
+++ js/script.js
@@ -2,6 +2,10 @@ const searchForm = document.querySelector('.search-form');
 const shoppinCart = document.querySelector('.shopping-cart');
 const cartLogin = document.querySelector('.cart-login');
 const navbar = document.querySelector('.navbar');
+
+
+/* ============ style for header section=================== */
+
 // to toggle the  search form bix
 document.querySelector('#search-btn').onclick = () => {
     searchForm.classList.toggle('active');
@@ -32,14 +36,54 @@ document.querySelector('#menu-btn').onclick = () => {
 
 }
 
-// document.addEventListener('click', () => {
-//     searchForm.classList.remove('active');
-//     shoppinCart.classList.remove('active');
-//     console.log("local click")
-// })
+
+const boxs = document.querySelectorAll('.box');
+const moreContent = document.querySelector('.moreContent');
+
+// show the readmore function
+function showReadMore(btnId, btnClass) {
+    btnId.onclick = () => {
+        btnClass.classList.toggle('active');
+        boxs.forEach((box) => {
+            box.classList.add('hide')
+        });
+    }
+}
+
 window.onscroll = () => {
     searchForm.classList.remove('active');
     shoppinCart.classList.remove('active');
     cartLogin.classList.remove('active');
     navbar.classList.remove('active');
 }
+
+/* ============ style for readmore and readless=================== */
+
+// show the quality feature
+const qualityFeatureBtn = document.querySelector('#qualityFeatureBtn');
+const qualityFeature = document.querySelector('#qualityFeature');
+showReadMore(qualityFeatureBtn, qualityFeature)
+
+// show the free delivery feature
+const freeDeliveryBtn = document.querySelector('#freeDeliveryBtn');
+const freeDelivery = document.querySelector('#freeDelivery');
+showReadMore(freeDeliveryBtn, freeDelivery)
+
+// show the easy payement feature
+const ePaymentBtn = document.querySelector('#ePaymentBtn');
+const ePayment = document.querySelector('#ePayment');
+showReadMore(ePaymentBtn, ePayment);
+
+// function for readLess
+const readLess = document.querySelectorAll('.readLess')
+readLess.forEach((read) => {
+    read.onclick = () => {
+        moreContent.classList.remove('active');
+        freeDelivery.classList.remove('active');
+        ePayment.classList.remove('active');
+        boxs.forEach((box) => {
+            box.classList.remove('hide')
+        })
+    }
+})
+
