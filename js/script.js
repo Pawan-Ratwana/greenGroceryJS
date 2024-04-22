4ac3389561b3b234554f10ae60f38780e112ef8f
Pawan Kumar <ratwanapawan@gmail.com>
2024-04-22 13:19:38 +0530

    complete the html and css home section

--- js/script.js
+++ js/script.js
@@ -1,17 +1,35 @@
 const searchForm = document.querySelector('.search-form');
 const shoppinCart = document.querySelector('.shopping-cart');
 const cartLogin = document.querySelector('.cart-login');
+const navbar = document.querySelector('.navbar');
 // to toggle the  search form bix
 document.querySelector('#search-btn').onclick = () => {
     searchForm.classList.toggle('active');
+    shoppinCart.classList.remove('active');
+    cartLogin.classList.remove('active');
+    navbar.classList.remove('active');
 }
 
 document.querySelector('#cart-btn').onclick = () => {
-    console.log("Shopping cart click")
-    shoppinCart.classList.toggle('active')
+    shoppinCart.classList.toggle('active');
+    cartLogin.classList.remove('active');
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
 }
 document.querySelector('#login-btn').onclick = () => {
     cartLogin.classList.toggle('active')
+    navbar.classList.remove('active')
+    shoppinCart.classList.remove('active')
+    searchForm.classList.remove('active')
+}
+
+
+document.querySelector('#menu-btn').onclick = () => {
+    navbar.classList.toggle('active');
+    cartLogin.classList.remove('active');
+    shoppinCart.classList.remove('active');
+    searchForm.classList.remove('active');
+
 }
 
 // document.addEventListener('click', () => {
@@ -19,7 +37,9 @@ document.querySelector('#login-btn').onclick = () => {
 //     shoppinCart.classList.remove('active');
 //     console.log("local click")
 // })
-document.addEventListener('scroll', () => {
+window.onscroll = () => {
     searchForm.classList.remove('active');
     shoppinCart.classList.remove('active');
-})
+    cartLogin.classList.remove('active');
+    navbar.classList.remove('active');
+}
